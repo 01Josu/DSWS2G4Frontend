37d48b4bfe3b69270f662aba5e0171e7432f2fcf
troopyux <aaronoliveral6@outlook.com>
2025-07-09 17:03:19 -0500

    configuracion backend render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1'
+  // apiBaseUrl: 'http://localhost:8080/api/v1'
+  apiBaseUrl: 'https://dsws2g4backend.onrender.com/api/v1'
 };
